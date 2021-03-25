07df4bd696b14da7a257b5bc9139b042893c9dcf
Pete Cornish <outofcoffee@gmail.com>
2021-03-25 21:37:09 +0000

    Improves ISO-8601 date/time parsing.

--- src/main/kotlin/io/gatehill/governor/util/SerialisationUtil.kt
+++ src/main/kotlin/io/gatehill/governor/util/SerialisationUtil.kt
@@ -2,9 +2,13 @@ package io.gatehill.governor.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 object SerialisationUtil {
-    val jsonMapper = ObjectMapper().registerKotlinModule()
-    val yamlMapper = YAMLMapper().registerKotlinModule()
+    val jsonMapper = ObjectMapper().customiseMapper()
+    val yamlMapper = YAMLMapper().customiseMapper()
+
+    private fun ObjectMapper.customiseMapper(): ObjectMapper =
+        this.registerKotlinModule().registerModule(JavaTimeModule())
 }
